1933233617a095075e51c064882cff2ef5ce67a6
abdulla <abdullaalasiri94@gmail.com>
2026-02-17 23:37:34 +0300

    Initial commit - Fursan Al-Fajr

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: { 
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    secure: process.env.NODE_ENV === 'production',
+    secure: true, // Always secure on Render
     httpOnly: true,
     sameSite: 'lax'
   }
